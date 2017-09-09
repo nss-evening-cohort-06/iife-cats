e347dc48a895947251550291a2ac6039e3b5aa51
Zoe Ames <zoeames@gmail.com>
2017-09-09 14:05:44 -0500

    done

--- javascripts/main.js
+++ javascripts/main.js
@@ -7,7 +7,24 @@ document.getElementById("fancyPetStore").addEventListener("click", (event) =>{
 	}
 });
 
+document.getElementById("all").addEventListener("click", (event) =>{
+	let cats = CatStore.getCats();
+	let owners = CatStore.getOwners();
+	CatStore.combineArrays(cats, owners);
+});
+
+document.getElementById("none").addEventListener("click", (event) =>{
+	CatStore.getCatsByOwner(0);
+});
+
+document.getElementById("zoe").addEventListener("click", (event) =>{
+	CatStore.getCatsByOwner(1);
+});
 
 document.getElementById("callan").addEventListener("click", (event) =>{
 	CatStore.getCatsByOwner(2);
+});
+
+document.getElementById("lauren").addEventListener("click", (event) =>{
+	CatStore.getCatsByOwner(3);
 });

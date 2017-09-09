22dda3a8aef5b0bbf2516593956084043db197ce
Zoe Ames <zoeames@gmail.com>
2017-09-09 11:31:34 -0500

    dom displaying

--- javascripts/cats.js
+++ javascripts/cats.js
@@ -1,5 +1,12 @@
-// var CatStore = ((oldCatStore) => {
+var CatStore = ((oldCatStore) => {
+  let cats = [];
 
+  oldCatStore.getCats = () => {
+  	return cats;
+  }
+  oldCatStore.setAllCats = (allMyCats) =>{
+  	cats = allMyCats;
+  };
 
-// 	return oldCatStore;
-// })(CatStore || {});
+	return oldCatStore;
+})(CatStore || {});
